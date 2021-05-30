91607c48c1e973f75779ce42e3ecfe337914f128
pramodkarki12 <np03a190229@heraldcollege.edu.np>
2021-05-30 23:21:29 +0545

    Creating a room id

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express();
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.status(200).send('hello World');
+  res.redirect(`/${uuidv4()}`);
 });
 
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 server.listen(3030);
