b01956199caf03c814f85254d1e2d6b1fd0eef2e
pramodkarki12 <np03a190229@heraldcollege.edu.np>
2021-05-30 23:55:22 +0545

    Adding the ability to view our own Video

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const server = require('http').Server(app);
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
