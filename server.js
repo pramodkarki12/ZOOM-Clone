ef7cc7ef102d717b7ee4c13ffdf455fa629590ee
pramodkarki12 <np03a190229@heraldcollege.edu.np>
2021-06-01 18:32:33 +0545

    Project Completed

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-
 const app = express();
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+const { v4: uuidV4 } = require('uuid');
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
-server.listen(3030);
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
